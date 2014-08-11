0326aa8c8635ebaa5d90940fc0cfca9ef1341951
blukat29 <yunjong@kaist.ac.kr>
2014-08-11 14:47:50 +0900

    [mode] Interactive mode display with animation.

--- include/main.js
+++ include/main.js
@@ -1,8 +1,10 @@
 
+// Save command override.
 CodeMirror.commands.save = function() {
   alert("saved.");
 };
 
+// Initialize vim.
 var editor = CodeMirror.fromTextArea(document.getElementById("vim"), {
   lineNumbers: true,
   mode: "text/x-csrc",
@@ -12,6 +14,7 @@ var editor = CodeMirror.fromTextArea(document.getElementById("vim"), {
   lineWrapping: true,
 });
 
+// Key buffer display.
 var keybuf = ''
 CodeMirror.on(editor, 'vim-keypress', function(e) {
   keybuf = keybuf + e;
@@ -22,7 +25,26 @@ CodeMirror.on(editor, 'vim-command-done', function(e) {
   $("#command-display").html(keybuf);
 });
 
+// Mode display.
+function mode_change(mode) {
+  $("#mode-display").html(mode);
+
+  var target={};
+  target['normal'] = $("#mode-normal").offset();
+  target['insert'] = $("#mode-insert").offset();
+  target['visual'] = $("#mode-visual").offset();
+
+  var offset_now = $("#mode-selector").offset();
+  var dt = target[mode].top - offset_now.top;
+
+  $("#mode-selector").animate({top:"+="+dt}, 100);
+}
 CodeMirror.on(editor, 'vim-mode-change', function(e) {
-  $("#mode-display").html(e['mode']);
+  mode_change(e['mode']);
+});
+
+// Initialize the site.
+$(document).ready(function(){
+  mode_change('normal');
 });
 

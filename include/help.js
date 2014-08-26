52e03b1a5d50069637d2c2d3010bec8c11fcf33d
blukat29 <yunjong@kaist.ac.kr>
2014-08-26 22:35:29 +0900

    [help] Display helps in a single set of commands.

--- include/help.js
+++ include/help.js
@@ -1,4 +1,7 @@
 
+var state_display = $("#state-display");
+var helps_display = $("#helps-display");
+
 var commandListEN = [
   { keys:['w'],     type:'motion',   help:'a word' },
   { keys:['d'],     type:'operator', help:'Delete' },
@@ -14,7 +17,19 @@ function CommandHelper (commandList) {
       { name:'motion',   from:'_operator', to:'_motion'   },
       { name:'operator', from:'_none',     to:'_operator' },
       { name:'done',     from:'*',         to:'_none'     },
-    ]});
+    ],
+    callbacks: {
+      onenterstate: function(e, from, to) {
+        state_display.html(from + " -> " + to);
+      },
+      on_motion: function(e, from, to, command) {
+        helps_display.append("<br>"+command.help);
+      },
+      on_operator: function(e, from, to, command) {
+        helps_display.append("<br>"+command.help);
+      }
+    },
+  });
   this.commandList = commandList;
 }
 CommandHelper.prototype = {
@@ -28,18 +43,21 @@ CommandHelper.prototype = {
       command = commandList[i];
       if (command.keys[0] == key && fsm.can(command.type)) {
         matchList.push(command);
+        console.log(command.type);
       }
     }
 
+    var match;
     if (matchList.length == 0) {
       throw ("No match found:"+key)
     }
     else if (matchList.length == 1) {
-      fsm[command.type];
-      return matchList[0];
+      match = matchList[0];
+      fsm[match.type](match);
     }
     else
       throw ("More than one commands match:"+key)
+    return match;
   }
 }
 
